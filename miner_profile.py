a25ca1b672ce6c0eb8dee5319891e217a2b84864
Hermano Ferreira <hermano.ferreira@lancium.com>
2022-09-14 11:35:24 -0500

    initial analysis

---
+++ miner_profile.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+import seaborn as sns
+
+from pandas_profiling import ProfileReport
+
+df = pd.read_csv('2021-Oct-Indicator-values.csv')
+
+
